21655a657a9439e4609103a78303365f94a1871a
Artur Miller <amiller89@gmx.de>
2020-08-02 17:45:19 +0200

    Removed accidently added comments

--- classes/scripts/pets.py
+++ classes/scripts/pets.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 from dog import Dog, Cat
-#import pdb
-#pdb.set_trace()
-#/home/amiller/workspace/devel/lib/python3/dist-packages/dog.cpython-38-x86_64-linux-gnu.so
+
+
 if __name__ == '__main__':
     dog = Dog('Bello')
     print(dog)
